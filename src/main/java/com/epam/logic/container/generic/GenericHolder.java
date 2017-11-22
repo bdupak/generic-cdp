d5147e51a8f3decac37a8e29a18e98d703934dfa
bdupak <bdupak@gmail.com>
2017-11-22 23:08:25 +0200

    added logic for GenericHolder

--- src/main/java/com/epam/logic/container/generic/GenericHolder.java
+++ src/main/java/com/epam/logic/container/generic/GenericHolder.java
@@ -1,4 +1,73 @@
 package com.epam.logic.container.generic;
 
-public class GenericHolder {
+import java.util.Iterator;
+
+public class GenericHolder<T> {
+
+    private static final int DEFAULT_CAPACITY = 10;
+    private int capacity = 0;
+    private int size = 0;
+    private Object[] holder;
+    private Iterator<T> iterator;
+
+    public GenericHolder() {
+        initHolder(DEFAULT_CAPACITY);
+    }
+
+    public GenericHolder(int capacity) {
+        initHolder(capacity);
+    }
+
+    private void initHolder(int capacity) {
+        if (capacity >= 0) {
+            holder = new Object[capacity];
+        } else {
+            throw new NegativeArraySizeException("The size of container can't be negative");
+        }
+    }
+
+    public void addElement(T element) {
+        if (size == capacity)
+            resize(holder);
+        holder[size] = element;
+        size++;
+    }
+
+    public void resize(Object[] oldHolder) {
+        capacity = capacity + (capacity >> 1);
+        System.arraycopy(holder, 0, oldHolder, 0, capacity);
+    }
+
+    public Object getElement(int index) {
+        if (index < size && index >= 0) {
+            return holder[index];
+        } else if (index > size) {
+            throw new IndexOutOfBoundsException(String.format("The holder has %s elements but not %s", size, index));
+        } else if (index < 0) {
+            throw new IndexOutOfBoundsException("You can use negative index");
+        }
+        throw new IllegalArgumentException("Oops something goes wrong");
+    }
+
+    public boolean findElement(T element) {
+        for(Object entity : holder) {
+            if (entity.equals(element)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int getIndex(T element) {
+        for(int i = 0; i < size; i++) {
+            if (holder[i].equals(element)) {
+                return i;
+            }
+        }
+        throw new IllegalArgumentException("No such element in container");
+    }
+
+    public int size() {
+        return size;
+    }
 }
