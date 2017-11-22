1c6d24f031d0554f5f71e285a6a911e1cb0ec297
bdupak <bdupak@gmail.com>
2017-11-23 00:29:10 +0200

    added logic to simple holder

--- src/main/java/com/epam/Main.java
+++ src/main/java/com/epam/Main.java
@@ -1,35 +1,53 @@
 package com.epam;
 
 import com.epam.logic.container.generic.GenericHolder;
+import com.epam.logic.container.without_generic.SimpleHolder;
 import com.epam.model.User;
 
 public class Main {
     public static void main(String[] args) {
 
+//        for check generic
+
         GenericHolder<User> users = new GenericHolder<>();
         for(int i = 0; i < 100; i++) {
-            users.addElement(new User("user" + i, "user" + 1, i));
+            users.addElement(new User("user" + i, "user" + i, i));
         }
         for(int i = 0; i < users.size(); i++) {
             System.out.println(users.getElement(i));
         }
-        System.out.println(users.getIndex(new User("user1", "user1", 1)));
-        System.out.println(users.findElement(new User("user1", "user1", 1)));
+        System.out.println("Index of user " + users.getIndex(new User("user1", "user1", 1)));
+        System.out.println("Find user " + users.findElement(new User("user1", "user1", 1)));
 
         GenericHolder<User> holder = new GenericHolder<>(200);
         for(int i = 0; i < 100; i++) {
-            holder.addElement(new User("user" + i, "user" + 1, i));
+            holder.addElement(new User("user" + i, "user" + i, i));
         }
         for(int i = 0; i < holder.size(); i++) {
             System.out.println(holder.getElement(i));
         }
-        System.out.println(holder.getIndex(new User("user1", "user1", 1)));
-        System.out.println(holder.findElement(new User("user1", "user1", 1)));
+        System.out.println("Index of user " + holder.getIndex(new User("user1", "user1", 1)));
+        System.out.println("Find user " + holder.findElement(new User("user1", "user1", 1)));
 
 
         for (User user : holder) {
             System.out.println("foreach " + user);
         }
 
+//        for check simple object holder
+        SimpleHolder simpleHolder = new SimpleHolder(200);
+        for(int i = 0; i < 10; i++) {
+            simpleHolder.addElement(new User("user" + i, "user" + i, i));
+        }
+        for(int i = 0; i < simpleHolder.size(); i++) {
+            System.out.println(simpleHolder.getElement(i));
+        }
+        System.out.println("Index of user " + simpleHolder.getIndex(new User("user2", "user2", 2)));
+        System.out.println("Find user " + simpleHolder.findElement(new User("user2", "user2", 2)));
+
+
+        for (Object user : simpleHolder) {
+            System.out.println("foreach " + user);
+        }
     }
 }
--- src/main/java/com/epam/logic/container/without_generic/SimpleHolder.java
+++ src/main/java/com/epam/logic/container/without_generic/SimpleHolder.java
@@ -1,4 +1,110 @@
 package com.epam.logic.container.without_generic;
 
-public class SimpleHolder {
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+
+public class SimpleHolder implements Iterable {
+    private static final int DEFAULT_CAPACITY = 10;
+    private int capacity = 0;
+    private int size = 0;
+    private Object[] holder;
+    private Iterator iterator;
+
+    public SimpleHolder() {
+        initHolder(DEFAULT_CAPACITY);
+    }
+
+    public SimpleHolder(int capacity) {
+        initHolder(capacity);
+    }
+
+    private void initHolder(int capacity) {
+        if (capacity >= 0) {
+            holder = new Object[capacity];
+            this.capacity = capacity;
+        } else {
+            throw new NegativeArraySizeException("The size of container can't be negative");
+        }
+    }
+
+    public void addElement(Object element) {
+        if (size == capacity)
+            resize();
+        holder[size] = element;
+        size++;
+    }
+
+    public void resize() {
+        capacity = capacity + (capacity >> 1);
+        holder = Arrays.copyOf(holder, capacity);
+    }
+
+    public Object getElement(int index) {
+        if (index < size && index >= 0) {
+            return holder[index];
+        } else if (index > size) {
+            throw new IndexOutOfBoundsException(String.format("The holder has %s elements but not %s", size, index));
+        } else if (index < 0) {
+            throw new IndexOutOfBoundsException("You can use negative index");
+        }
+        throw new IllegalArgumentException("Oops something goes wrong");
+    }
+
+    public boolean findElement(Object element) {
+        for (Object entity : holder) {
+            if (entity.equals(element)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int getIndex(Object element) {
+        for (int i = 0; i < size; i++) {
+            if (holder[i].equals(element)) {
+                return i;
+            }
+        }
+        throw new IllegalArgumentException("No such element in container");
+    }
+
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return new CustomIterator();
+    }
+
+    @Override
+    public void forEach(Consumer action) {
+        for (int i = 0; i < size; i++) {
+            action.accept(holder[i]);
+        }
+    }
+
+    @Override
+    public Spliterator spliterator() {
+        return null;
+    }
+
+    public class CustomIterator implements Iterator {
+        int position = -1;
+        int lastReturned;
+
+        @Override
+        public boolean hasNext() {
+            return position != size - 1;
+        }
+
+        @Override
+        public Object next() {
+            position++;
+            lastReturned = position;
+            return holder[position];
+        }
+    }
 }
